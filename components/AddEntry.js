0b6817abac308641844e12f36e4483a94deb6533
Gabriel Nogueira <gfnogueira@gmail.com>
2018-09-05 08:07:14 -0300

    display already logged info

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,7 +1,9 @@
+import { Ionicons } from '@expo/vector-icons';
 import React, { Component } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 import { getMetricMetaInfo, timeToString } from '../utils/helpers';
 import DateHeader from './DateHeader';
+import TextButton from './TextButton';
 import UdaciSlider from './UdaciSlider';
 import UdaciSteppers from './UdaciSteppers';
 
@@ -74,8 +76,28 @@ class AddEntry extends Component {
     // Clear local notification
   };
 
+  reset = () => {
+    const key = timeToString();
+
+    // Update Redux
+
+    // Route to Home
+
+    // Update "DB"
+  };
+
   render() {
     const metaInfo = getMetricMetaInfo();
+
+    if (this.props.alreadyLogged) {
+      return (
+        <View>
+          <Ionicons name="ios-happy-outline" size={100} />
+          <Text>You already logged your information for today</Text>
+          <TextButton onPress={this.reset}>Reset</TextButton>
+        </View>
+      );
+    }
 
     return (
       <View>

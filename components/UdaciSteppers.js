0b1931dcce9fc5b5d2f99fad3b24611793717d09
Gabriel Nogueira <gfnogueira@gmail.com>
2018-09-04 23:31:01 -0300

    add functionality to UdaciSteppers

--- components/UdaciSteppers.js
+++ components/UdaciSteppers.js
@@ -1,10 +1,22 @@
+import { FontAwesome } from '@expo/vector-icons';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 
-const UdaciSteppers = () => {
+const UdaciSteppers = ({ max, unit, step, value, onIncrement, onDecrement }) => {
   return (
     <View>
-      <Text>UdaciSteppers</Text>
+      <View>
+        <TouchableOpacity onPress={onDecrement}>
+          <FontAwesome name="minus" size={30} color={'black'} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onIncrement}>
+          <FontAwesome name="plus" size={30} color={'black'} />
+        </TouchableOpacity>
+      </View>
+      <View>
+        <Text>{value}</Text>
+        <Text>{unit}</Text>
+      </View>
     </View>
   );
 };

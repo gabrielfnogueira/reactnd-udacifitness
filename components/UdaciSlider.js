66a94c232df6cd053ee53d345ffffc54dc816d59
Gabriel Nogueira <gfnogueira@gmail.com>
2018-09-04 23:25:27 -0300

    use react-native Slider in UdaciSlider

--- components/UdaciSlider.js
+++ components/UdaciSlider.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Slider, Text, View } from 'react-native';
 
-const UdaciSlider = () => {
+const UdaciSlider = ({ max, unit, step, value, onChange }) => {
   return (
     <View>
-      <Text>UdaciSlider</Text>
+      <Slider step={step} value={value} maximumValue={max} minimumValue={0} onValueChange={onChange} />
+      <View>
+        <Text>{value}</Text>
+        <Text>{unit}</Text>
+      </View>
     </View>
   );
 };

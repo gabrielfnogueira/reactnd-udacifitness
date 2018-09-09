b3d78db6366c6b9d0cf1408e7168c5482d48047c
Gabriel Nogueira <gfnogueira@gmail.com>
2018-09-09 17:01:33 -0300

    create MetricCard component

--- components/History.js
+++ components/History.js
@@ -7,6 +7,7 @@ import { fecthCalendarResults } from '../utils/api';
 import { white } from '../utils/colors';
 import { getDailyReminderValue, timeToString } from '../utils/helpers';
 import DateHeader from './DateHeader';
+import MetricCard from './MetricCard';
 
 class History extends Component {
   componentDidMount() {
@@ -34,7 +35,7 @@ class History extends Component {
         </View>
       ) : (
         <TouchableOpacity onPress={() => console.log('Pressed!')}>
-          <Text>{JSON.stringify(metrics)}</Text>
+          <MetricCard metrics={metrics} date={formattedDate} />
         </TouchableOpacity>
       )}
     </View>

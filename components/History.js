c08a66d40449369a195b506697ebe04aab1159ac
Gabriel Nogueira <gfnogueira@gmail.com>
2018-09-09 16:40:23 -0300

    add UdaciFitnessCalendar dependency

--- components/History.js
+++ components/History.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { receiveEntries, addEntry } from '../actions';
 import { fecthCalendarResults } from '../utils/api';
 import { timeToString, getDailyReminderValue } from '../utils/helpers';
+import UdaciFitnessCalendar from 'udacifitness-calendar';
 
 class History extends Component {
   componentDidMount() {
@@ -22,12 +23,22 @@ class History extends Component {
       });
   }
 
-  render() {
+  renderItem = ({ today, ...metrics }, formattedDate, key) => (
+    <View>{today ? <Text>{JSON.stringify(today)}</Text> : <Text>{JSON.stringify(metrics)}</Text>}</View>
+  );
+
+  renderEmptyDate(formattedDate) {
     return (
       <View>
-        <Text>{JSON.stringify(this.props)}</Text>
+        <Text>No data for this day</Text>
       </View>
     );
+  }
+
+  render() {
+    const { entries } = this.props;
+
+    return <UdaciFitnessCalendar items={entries} renderItem={this.renderItem} renderEmptyDate={this.renderEmptyDate} />;
   }
 }
 
